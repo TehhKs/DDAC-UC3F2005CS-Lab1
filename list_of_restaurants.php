56a129ecb297c109474b4786db52e0692a1bd60f
TehhKs <72076799+TehhKs@users.noreply.github.com>
2020-10-09 18:33:45 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -21,6 +21,40 @@
           <th>Menara Standard Chartered, TPM</th>
         <th>03-22441234</th>
       </tr>
+          
+      <?php
+      $connectionInfo = 
+          array(
+          "UID" => "TEHKAISHENG", 
+          "pwd" => "{@TP050590}", 
+          "Database" => "lab2databasecloudserver", 
+          "LoginTimeout" => 30, 
+          "Encrypt" => 1, 
+          "TrustServerCertificate" => 0);
+      $serverName = "tcp:lab2databaseserver.database.windows.net,1433";
+      $conn = sqlsrv_connect($serverName, $connectionInfo);     
+          if (!$conn)
+            {
+              die("Error connection: ".sqlsrv_errors());
+            }
+
+            $tsql= "SELECT * FROM [dbo].[restaurant]";
+            $getResults= sqlsrv_query($conn, $tsql);
+              if ($getResults == FALSE) 
+              {
+               die(sqlsrv_errors());
+              }
+              while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+                {
+                   echo "<tr>";
+                   echo "<td>". $row['restaurant_id'] . "</td>";
+                   echo "<td>". $row['restaurant_name'] ."</td>";
+                   echo "<td>". $row['restaurant_address'] . "</td>";
+                   echo "<td>". $row['restaurant_phone'] . "</td>";
+                   echo "</tr>";
+                 }
+                 sqlsrv_free_stmt($getResults);
+        ?>      
       </table>
   </center>
  </body>
